fcf88ee3e3ebf8c006aaf35bdce23392a0ce3419
electrobullet <dmitry.pigasin@yandex.ru>
2022-12-15 17:09:48 +0300

    Remove `os` and `sys` from `ctx.py`

--- app/ctx.py
+++ app/ctx.py
@@ -1,16 +1,13 @@
-import os
-import sys
-
 from dnn.opencv.EfficientFER import EfficientFER
 from dnn.opencv.FaceDetectionRetail0044 import FaceDetectionRetail0044
 
 face_detector = FaceDetectionRetail0044(
-    os.path.join(os.path.dirname(sys.argv[0]), 'models', 'face-detection-retail-0044.caffemodel'),
-    os.path.join(os.path.dirname(sys.argv[0]), 'models', 'face-detection-retail-0044.prototxt'),
+    'models/face-detection-retail-0044.caffemodel',
+    'models/face-detection-retail-0044.prototxt',
 )
 
 emotion_recognizer = EfficientFER(
-    os.path.join(os.path.dirname(sys.argv[0]), 'models', '5_classes_EfficientNetV2B1_96x96_bs_256_weighted.onnx'),
+    'models/EfficientFER5_96x96.onnx',
     (96, 96),
     emotions=[
         'anger',
